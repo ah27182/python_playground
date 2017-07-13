35682b0709563183b2ce040abbefa70aa349fde0
Ahmed Husain <ah27182@berkeley.edu>
2017-07-13 03:29:15 -0700

    added substring and popper files

---
+++ popper.py
@@ -0,0 +1,18 @@
+from sets import Set
+
+# Function that removes an input of indices from an array
+# ex. popper([0,1,2,3,4], [1,0,2]) will remove el
+
+def popper(arr, indicesToRemove):
+	s = Set()
+
+	for index in indicesToRemove:
+		for num in s:
+			if index > num:
+				index -= 1
+		arr.pop(index)
+		s.add(index)
+
+	return arr
+
+popper([0,1,2,3,4], [4,2,3])
